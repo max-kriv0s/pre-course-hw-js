b6ddc8cc5ca5bdfe7ac406a9c90c9e9518efaa5b
Maxim Krivoshein <max.kriv0s@gmail.com>
2024-04-12 00:01:44 +0300

    homework-02-05

---
+++ lesson-02/05.js
@@ -0,0 +1,9 @@
+let passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+const passportCopy = {...passport};
+passportCopy.name = "Ivan";
+
+console.log(passport.name);
+console.log(passportCopy.name);
