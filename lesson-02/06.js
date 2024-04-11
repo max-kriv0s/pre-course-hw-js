9c66b337342ff0360f2bcea3c9a7ed19d8cb456f
Maxim Krivoshein <max.kriv0s@gmail.com>
2024-04-12 00:07:33 +0300

    homework-02-06

---
+++ lesson-02/06.js
@@ -0,0 +1,13 @@
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+const passportWithAddressCopy = JSON.parse(JSON.stringify(passportWithAddress));
+passportWithAddressCopy.address.city = "Bobryisk";
+
+console.log(passportWithAddress.address.city);
+console.log(passportWithAddressCopy.address.city);
